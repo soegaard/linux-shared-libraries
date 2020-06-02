d7fa0ce043e20c672f478787c3b472ef04a59d5d
Jens Axel Søgaard <jensaxel@soegaard.net>
2020-06-02 18:05:51 +0200

    Add info files

---
+++ flomat-x86_64-linux-natipkg/info.rkt
@@ -0,0 +1,8 @@
+#lang setup/infotab
+
+(define install-platform "x86_64-linux-natipkg")
+
+(define copy-foreign-libs '("libblas.so.3"
+                            "libgfortran.so.3"
+                            "liblapack.so.3"
+                            "libquadmath.so.3"))
---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang setup/infotab
+
+(define collection 'multi)
+(define deps '("base"))
+
+(define pkg-desc "native libraries for \"flomat\" package")
+
+(define pkg-authors '(soegaard))
